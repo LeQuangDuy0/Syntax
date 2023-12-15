abd583ebaff0e72922dbc721f3f017d55c18198b
KyoDyy <144084641+LeQuangDuy0@users.noreply.github.com>
2023-12-16 02:11:53 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,32 @@
+package basic.dev;
+
+/**
+ * ghi chú (Comment nhiều dòng)
+ * 
+ * @author TranThanhBinh 1. Tên lớp trùng tên file 2. Tên lớp chữ cái đầu viết
+ *         hoa. Nếu từ ghép thì những chữ cái đầu viết hoa 3. Tên lớp đặt phải
+ *         có ý nghĩa. Ví dụ: class Peson : lớp mô tả con người - Iterface: phải
+ *         có tiền tố I. Ví dụ: IListener - Abstract: Phải có tiền tố Abstract.
+ *         Ví dụ: AbstractShape
+ */
+public class MainApp {
+	// Comment 1 dòng cách 1. Phím nóng ctr + /
+	/* Comment 1 dòng cách 2 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biến: 1. Không chứa ký tự đặc biệt !@#$%^&*()-+= 2. Không chứa
+		 * khoảng trắng 3. Viết thường 4. Không được bắt đầu bằng số
+		 */
+		int number;
+		String nameSchool;
+	}
+
+	/*
+	 * Tên phương thức tuân theo quy tắc camelcase
+	 */
+	public void show() {
+
+	}
+
+}
